b78e02630918c57f96d144fb775969032e791e7f
YFYang <yf9505@outlook.com>
2021-11-16 10:28:26 +0800

    更改为计算单时刻流场

--- SingleParticleAnalysis/main.cpp
+++ SingleParticleAnalysis/main.cpp
@@ -12,10 +12,19 @@ of equi data file & shear data file
 #include <boost/timer/timer.hpp>
 #include <boost/filesystem.hpp>
 
-int main()
+int main(int argc, size_t *argv[3])
 {
 	try
 	{
+		if (argc != 3)
+		{
+			throw std::exception("wrong args num!\n");
+		}
+		size_t timestep_t0 = *argv[1];
+		size_t delta_step = *argv[2];
+		std::cout << "t0 timestep: " << timestep_t0 << "\n";
+		std::cout <<"delta timstep: " << delta_step << '\n';
+		std::cout << "t timstep: " << delta_step + timestep_t0<< '\n';
 		std::ios_base::sync_with_stdio(false);
 		std::cin.tie(NULL);
 		boost::timer::auto_cpu_timer timer;
@@ -28,22 +37,18 @@ int main()
 		if (!f_settings.is_open())
 		{
 			std::cerr << "file \"PaAn_Settings.json\" open failed" << endl;
-			throw "file \"PaAn_Settings.json\" open failed";
+			throw std::exception("file \"PaAn_Settings.json\" open failed");
 		}
 		f_settings >> root;
 		double wi = root["wi"].asDouble();
 		double tau_alpha = root["tau_alpha"].asDouble();
 		double rate = wi / tau_alpha;
-		string t0_fname = root["t0config_fname"].asString();
-		size_t start_moment = root["start_moment"].asLargestUInt();
-		size_t moment_number = root["moment_number"].asLargestUInt();
-		size_t delta_step = root["delta_step"].asLargestUInt();
-		size_t t0_shift_timestep = root["t0_shift_step"].asLargestUInt();
+
 		string fname_prefix = root["fname_prefix"].asString();
 		string fname_postfix = root["fname_postfix"].asString();
 		string data_fpath = root["data_fpath"].asString();
 		string output_path = root["output_path"].asString();
-		string t0data_fpath = root["t0data_fpath"].asString();
+
 
 		string str_t0data_pairstyle = root["t0data_pairstyle"].asString();
 		Configuration::PairStyle t0data_pairstyle = Configuration::PairStyle::none;
@@ -61,8 +66,7 @@ int main()
 		}
 		else
 		{
-			cerr << ("WRONG pair style: " + str_t0data_pairstyle) << endl;
-			throw ("WRONG pair style: " + str_t0data_pairstyle);
+			throw std::exception(("WRONG pair style: " + str_t0data_pairstyle).c_str());
 		}
 
 		string str_t0data_boxtype = root["t0data_boxtype"].asString();
@@ -77,76 +81,59 @@ int main()
 		}
 		else
 		{
-			cerr << ("WRONG box type: " + str_t0data_boxtype) << endl;
-			throw ("WRONG box type: " + str_t0data_boxtype);
+			throw std::exception(("WRONG box type: " + str_t0data_boxtype).c_str());
 		}
 
-		string str_shear_data_pairstyle = root["shear_data_pairstyle"].asString();
-		Configuration::PairStyle shear_data_pairstyle = Configuration::PairStyle::none;
-		if (str_shear_data_pairstyle == "single")
+		string str_tdata_pairstyle = root["tdata_pairstyle"].asString();
+		Configuration::PairStyle tdata_pairstyle = Configuration::PairStyle::none;
+		if (str_tdata_pairstyle == "single")
 		{
-			shear_data_pairstyle = Configuration::PairStyle::single;
+			tdata_pairstyle = Configuration::PairStyle::single;
 		}
-		else if (str_shear_data_pairstyle == "pair")
+		else if (str_tdata_pairstyle == "pair")
 		{
-			shear_data_pairstyle = Configuration::PairStyle::pair;
+			tdata_pairstyle = Configuration::PairStyle::pair;
 		}
-		else if (str_shear_data_pairstyle == "none")
+		else if (str_tdata_pairstyle == "none")
 		{
-			shear_data_pairstyle = Configuration::PairStyle::none;
+			tdata_pairstyle = Configuration::PairStyle::none;
 		}
 		else
 		{
-			cerr << ("WRONG pair style: " + str_shear_data_pairstyle) << endl;
-			throw ("WRONG pair style: " + str_shear_data_pairstyle);
+			throw std::exception(("WRONG pair style: " + str_tdata_pairstyle).c_str());
+		}
+
+		string str_tdata_boxtype = root["tdata_boxtype"].asString();
+		auto tdata_boxsytle = Configuration::BoxType::orthogonal;
+		if (str_tdata_boxtype == "orthogonal")
+		{
+			tdata_boxsytle = Configuration::BoxType::orthogonal;
+		}
+		else if (str_t0data_boxtype == "tilt")
+		{
+			tdata_boxsytle = Configuration::BoxType::tilt;
+		}
+		else
+		{
+			throw std::exception(("WRONG box type: " + str_t0data_boxtype).c_str());
 		}
 
 
-		double CN_rcut = root["CN_rcut"].asDouble();
 
 
 		boost::filesystem::path boost_path_check(data_fpath);
 		if (!(boost::filesystem::exists(boost_path_check) && boost::filesystem::is_directory(boost_path_check)))
 		{
 			cerr << "data file path: " << boost_path_check << " not exits" << endl;
-			throw ("data file path: " + boost_path_check.string() + " not exits");
-		}
-		boost_path_check = boost::filesystem::path(t0data_fpath);
-		if (!(boost::filesystem::exists(boost_path_check) && boost::filesystem::is_directory(boost_path_check)))
-		{
-			cerr << "data file path: " << boost_path_check << " not exits" << endl;
-			throw ("data file path: " + boost_path_check.string() + " not exits");
+			throw std::exception(("data file path: " + boost_path_check.string() + " not exits").c_str());
 		}
 
 		void mkdir(std::string path);
 		mkdir(output_path);
 
-
-		Configuration_StaticStructure config_t0
-		(t0data_fpath + t0_fname, t0data_boxsytle, t0data_pairstyle);//Configuration::PairStyle::single);
-		config_t0.set_time_step(0 + t0_shift_timestep); //设定t0
-
-		bool flag_CN = root["computeCN"].asBool();
-		bool flag_MSD = root["computeMSD"].asBool();
-		bool flag_MSDnonAffine = root["computeMSDnonAffine"].asBool();
-		bool flag_MSDnonAffine_t0 = root["MSDnonAffine_t0"].asBool();
-		bool flag_MSDnonAffine_ave_gradient = root["MSDnonAffine_ave_gradient"].asBool();
 		bool flag_computeFlowDisplacement = root["computeFlowDisplacement"].asBool();
 		bool flag_computeFlowAveVelocity = root["computeFlowAveVelocity"].asBool();
-		string CNdir = output_path + "cn/";
-		if (flag_CN) mkdir(CNdir);
 
-		string MSDdir = output_path + "MSD/";
-		if (flag_MSD) mkdir(MSDdir);
-
-		string MSDnonAffinedir = output_path + "MSDnonAffine/";
-		if (flag_MSDnonAffine) mkdir(MSDnonAffinedir);
-
-		string MSDnonAffine_gradient_ave_dir = output_path + "MSDnonAffineGrdAve/";
-		if (flag_MSDnonAffine_ave_gradient) mkdir(MSDnonAffine_gradient_ave_dir);
-
-		string MSDnonAffine_t0_dir = output_path + "MSDnonAffine_t0/";
-		if (flag_MSDnonAffine_t0) mkdir(MSDnonAffine_t0_dir);
 
 		string flow_displacement_dir = output_path + "FlowDisplacement/";
 		string flow_displacement_non_cross_box_dir = output_path + "FlowDisplacement_nonCrossBox/";
@@ -162,106 +149,30 @@ int main()
 			mkdir(flow_ave_velocity_dir);
 		}
 
-		if (flag_CN)
+		string config_t0_fname = fname_prefix + to_string(timestep_t0) + fname_postfix;
+		Configuration_StaticStructure config_t0
+		(data_fpath + config_t0_fname, t0data_boxsytle, t0data_pairstyle);//Configuration::PairStyle::single);
+
+
+		string str_timestep = to_string(timestep_t0 + delta_step);
+		string config_t_fname = fname_prefix + str_timestep + fname_postfix;
+		Configuration_ParticleDynamic config_t
+		(data_fpath + config_t_fname, tdata_boxsytle, tdata_pairstyle);
+
+		if (flag_computeFlowDisplacement)//!计算单粒子流场方向位移，以进一步计算流场问题，可以淘汰之前剪切带程序。
 		{
-			config_t0.compute_CN(CN_rcut);
-			config_t0.CN_to_file(CNdir + "cn.0");
+
+			config_t.compute_shear_flow_displacement(config_t0, Configuration_ParticleDynamic::ShearDirection::xy);
+			config_t.to_file_flow_displacement(flow_displacement_dir + "FlowDisplacement." + str_timestep);
 		}
-		vector<double> y_sum(config_t0.get_particle().size(), 0);
-		vector<double> x_sum(config_t0.get_particle().size(), 0);
-		vector<double> z_sum(config_t0.get_particle().size(), 0);
-		for (size_t i = 0; i < config_t0.get_particle().size(); i++)
+
+		if (flag_computeFlowAveVelocity)
 		{
-			const Particle& pa = config_t0.get_particle()[i];
-			x_sum[pa.id] = pa.rx;
-			y_sum[pa.id] = pa.ry;
-			z_sum[pa.id] = pa.rz;
+				
+			config_t.compute_shear_flow_ave_velocity(config_t0, Configuration_ParticleDynamic::ShearDirection::xy);
+			config_t.to_file_flow_ave_velocity(flow_ave_velocity_dir + "FlowAveVelocity." + str_timestep);
 		}
-
-		size_t ncounter = 0;
-		Configuration_ParticleDynamic config_last_moment;
-		for (size_t imoment = start_moment; imoment <= moment_number; imoment++)
-		{
-			size_t istep = imoment * delta_step + t0_shift_timestep;
-			ncounter++;
-			string str_istep = to_string(istep);
-			string config_t_fname = data_fpath + fname_prefix + str_istep + fname_postfix;
-			Configuration_ParticleDynamic config_t(config_t_fname, Configuration::BoxType::tilt, shear_data_pairstyle);
-			if (flag_CN)
-			{
-				config_t.compute_CN(CN_rcut);
-				config_t.CN_to_file(CNdir + "cn." + str_istep);
-			}
-
-			if (flag_MSD)
-			{
-				config_t.compute_msd(config_t0);
-				config_t.to_file_MSD(MSDdir + "MSD." + str_istep);
-			}
-
-			if (flag_MSDnonAffine)
-			{
-				config_t.compute_shear_MSDnonAffine(config_t0, Configuration_ParticleDynamic::ShearDirection::xy, rate);
-				config_t.to_file_nonAffineMSD(MSDnonAffinedir + "MSDnonAffine." + str_istep);
-			}
-
-			if (flag_MSDnonAffine_t0)
-			{
-				config_t.compute_shear_MSDnonAffine_t0(config_t0, Configuration_ParticleDynamic::ShearDirection::xy, rate);
-				config_t.to_file_nonAffineMSD(MSDnonAffine_t0_dir + "MSDnonAffine_t0." + str_istep);
-			}
-
-			if (flag_MSDnonAffine_ave_gradient)
-			{
-				for (size_t i = 0; i < config_t.get_particle().size(); i++)
-				{
-					const Particle& pa = config_t.get_particle()[i];
-					x_sum[pa.id] += config_t.get_rx_real(pa);//pa.rx;
-					y_sum[pa.id] += config_t.get_ry_real(pa);// pa.ry;
-					z_sum[pa.id] += config_t.get_rz_real(pa);// pa.rz;
-				}
-				vector<double> y_ave = y_sum;
-				for (size_t i = 0; i < y_ave.size(); i++)
-				{
-					y_ave[i] /= ncounter;
-				}
-				config_t.compute_shear_MSDnonAffine(config_t0, Configuration_ParticleDynamic::ShearDirection::xy, rate, y_ave);
-				config_t.to_file_nonAffineMSD(MSDnonAffine_gradient_ave_dir + "MSDnonAffineGrdAve." + str_istep);
-			}
-
-			if (flag_computeFlowDisplacement)//!计算单粒子流场方向位移，以进一步计算流场问题，可以淘汰之前剪切带程序。
-			{
-				if (imoment == start_moment)
-				{
-					config_t.compute_shear_flow_displacement(config_t0, Configuration_ParticleDynamic::ShearDirection::xy);
-				}
-				else
-				{
-					config_t.compute_shear_flow_displacement(config_last_moment, Configuration_ParticleDynamic::ShearDirection::xy);
-				}
-				config_t.to_file_flow_displacement(flow_displacement_dir + "FlowDisplacement." + str_istep);
-			}
-
-			if (flag_computeFlowAveVelocity)
-			{
-				if (imoment == start_moment)
-				{
-					config_t.compute_shear_flow_ave_velocity(config_t0, Configuration_ParticleDynamic::ShearDirection::xy);
-				}
-				else
-				{
-					config_t.compute_shear_flow_ave_velocity(config_last_moment, Configuration_ParticleDynamic::ShearDirection::xy);
-				}
-				config_t.to_file_flow_ave_velocity(flow_ave_velocity_dir + "FlowAveVelocity." + str_istep);
-			}
-
-			bool flag_store_last_moment = flag_computeFlowAveVelocity || flag_computeFlowDisplacement;
-			if (flag_store_last_moment)
-			{
-				config_last_moment = config_t;
-				cout << config_last_moment.get_timestep() << '\n';
-			}
-		}
+		
 		cout << "####################################################################" << '\n';
 		return 0;
 	}
